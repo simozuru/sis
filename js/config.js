dbecf40fc52ddd6094cc0f4de23bb44a0fdbbd66
works <works226060@gmail.com>
2026-08-21 19:22:25 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
 
 // システム設定
 const CONFIG = {
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxTXwzWa_az6-TUx2CFvejmJrMIdXzfR-qZoCVn0409n-lubPAE_rGgdg-79oslG5LU/exec",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxAc8XZzPTCa_csad5_UVTBhr-PMCRBYuobXWOVV0vxME2mcmhmFzFk8e9pWFQnyIyA/exec",
   STORAGE_FIELDS: ["name", "name_kana", "tel", "email"],
   STORAGE_PREFIX: "sis_",
 
